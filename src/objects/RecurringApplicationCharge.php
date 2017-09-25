90cd40a2a6e21cebd1233e5fea76f574a353b6fe
Brandon schmidt <schmidt@xariable.com>
2017-09-25 18:11:04 -0400

    Added an activation function to the recurring billing module

--- src/objects/RecurringApplicationCharge.php
+++ src/objects/RecurringApplicationCharge.php
@@ -1,5 +1,6 @@
 <?php
 namespace Xariable\Shopify\Objects;
+use \Xariable\Shopify\Exceptions\ShopifyException;
 
 class RecurringApplicationCharge extends BaseObject {
 
@@ -7,4 +8,29 @@ class RecurringApplicationCharge extends BaseObject {
 
 	protected $name = "recurring_application_charges";
 	protected $key = "recurring_application_charge";
+
+	/**
+	* activate
+	*
+	* @param object - Recurring Application Charge
+	*/
+	public function activate( $charge ) {
+
+		$url = $this->getShopBaseUrl() ."/admin/{$this->name}/". $charge->id ."/activate.json";
+
+		$headers        = $this->getRequestHeaders();
+		$json           = json_encode($charge);
+		$data           = '{ "' . $this->key . '":' . $json . '}';
+
+		$res    = $this->execute( $url, "POST", $headers, $data );
+		$result = json_decode( $res );
+
+		if( $result and property_exists( $result, 'recurring_application_charges') ) {
+			if( count( $result->recurring_application_charges ) == 0 )
+				return "";
+
+			return json_encode( $result->{'recurring_application_charges'} );
+		}
+
+	}
 }
