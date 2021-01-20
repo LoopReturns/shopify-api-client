3570e97cbed10524e718b5fee9fe02c0fbd49ec4
Brian O'Connell <brian@loopreturns.com>
2021-01-20 11:17:58 -0500

    no tabs

--- src/objects/Customer.php
+++ src/objects/Customer.php
@@ -8,34 +8,35 @@
 
 namespace Xariable\Shopify\Objects;
 
-class Customer extends BaseObject {
+class Customer extends BaseObject
+{
 
-	use \Xariable\Shopify\Traits\ShopifyTransport;
+    use \Xariable\Shopify\Traits\ShopifyTransport;
 
-	protected $name = "customers";
-	protected $key = "customer";
+    protected $name = "customers";
+    protected $key = "customer";
 
-	# Pass in Array
-	public function search($args=array())
+    # Pass in Array
+    public function search($args = array())
     {
-		$url = $this->getShopBaseUrl() . "{$this->name}/search.json";
+        $url = $this->getShopBaseUrl() . "{$this->name}/search.json";
 
-		if (array_key_exists('query', $args)) {
+        if (array_key_exists('query', $args)) {
             $url .= "?query=" . $args['query'];
         }
 
         $headers = $this->getRequestHeaders();
 
-		$result = $this->execute($url, "GET", $headers);
-		$result = json_decode($result);
+        $result = $this->execute($url, "GET", $headers);
+        $result = json_decode($result);
 
-		if ($result and property_exists($result, 'customers')) {
-			if (count($result->customers) == 0) {
+        if ($result and property_exists($result, 'customers')) {
+            if (count($result->customers) == 0) {
                 return "";
             }
-			return json_encode($result->{'customers'});
-		}
+            return json_encode($result->{'customers'});
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 }
